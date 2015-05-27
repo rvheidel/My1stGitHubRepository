e7b889dc70dadfb2e3a004ccacdf26ddcf95bf35
rvheidel <rvheidel@gmail.com>
2015-05-26 20:20:13 -0400

    added about.php contact.php main.css

---
+++ contact.php
@@ -0,0 +1,45 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>The Golden Scoop</title>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<!-- <meta name="viewport" content="initial-scale=1, maximum-scale=1"> -->
+<meta name='viewport' content='width=640' />
+<link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+<div id="wrap">
+<div id="topBanner">
+	<img id='logo' src='image/goldenscroop.jpg' alt='' width='89' height='76' align='left' border='1 black' />
+	<img id='logo2' src='image/Rainbow.png' alt='' width='89' height='76' align='right' />
+	<h1>The Golden Scoop Shop</h1>
+	<h4> "Home of the perfectionist Ice Cream Makers"</h4>
+</div>
+<ul id="nav">
+	<li><a href="index.html">Home</a></li>
+	<li><a href="Ice_Cream.php">Ice Cream Flavors</a></li>
+	<li><a href="Frozen_Yogurt.php">Frozen Yogurt Flavors</a></li>
+    <li><a href="about.php">About Golden Scoop</a></li>
+    <li><a href="contact.php">Contact Us</a></li>
+</ul>
+<div id='content'>
+    <img id='shop' src='image/Icecreamshop1.jpg' alt='hello' width='197' height='167' align='left' />     
+    <h2 class="title">Contact The Golden Scoop</h2>
+    <p><b>PHONE:</b> 555-555-5555</p>
+	<p><b>E-MAIL:</b> thegoldenscoop@example.com</p>
+	
+	</p>
+	If you like you can also purchase our products yourself on <a href="http://www.amazon.com">Amazon.</a>
+    </div>
+
+<div id='footer'>
+	<!-- <img src='image/goldenscroop.jpg' alt='hello' width='179' height='152' align='center' /> -->
+	<h4>I</h4>
+	</div>
+<div id='copyright'>
+	<h4>Copyright 2015 | WEB260 Class Project by Robert V Heidel</h4>
+	</div>
+</div>
+</body>
+</html>
